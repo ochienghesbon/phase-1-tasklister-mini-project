3aaaabe68af290c72c3674dc71bf8db7e5d453d0
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-13 17:18:27 +0300

    adds js content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.querySelector('form').addEventListener('submit', (e)=>{
-    e.preventDefault()
-  })
+  const submit = document.querySelector('form');
+    submit.addEventListener("submit", (e) => {
+        e.preventDefault()
+        createToDo()
+        submit.reset()
+    })
+
+    function createToDo() {
+        const input = document.getElementById("new-task-description");
+        const newTask = document.createElement('li');
+        const deleteBtn = document.createElement('button');
+
+        newTask.textContent = `${input.value}       `;
+        document.getElementById('tasks').appendChild(newTask);
+
+
+        deleteBtn.textContent = "DONE";
+        newTask.appendChild(deleteBtn);
+        deleteBtn.addEventListener('click', () => newTask.remove());
+
+    }
 });
